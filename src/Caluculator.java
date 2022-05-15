13d18cad36b70192dcd664b54e6b10f61a076e93
shasap <sapan.shah.qa@gmail.com>
2022-05-15 11:59:04 +0100

    added sub method

--- src/Caluculator.java
+++ src/Caluculator.java
@@ -1,6 +1,10 @@
 public class Caluculator {
     public static void add(int a, int b){
         System.out.println(a+b);
+        ///
 
+    }
+    public static void sub(int a, intb){
+        System.out.println(a-b);
     }
 }
